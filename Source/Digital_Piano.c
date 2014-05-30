b54e189df372fd33d7f67a12f8246b177cb5ee3f
btstevenson <bstevo30@gmail.com>
2014-05-29 22:50:57 -0700

    tested functionality of EEPROM

--- Source/Digital_Piano.c
+++ Source/Digital_Piano.c
@@ -10,14 +10,17 @@
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
-const char *Welcome[3] = {"Welcome to the  Digital Piano.", "Can play, record Press A0 to", "continue."};
-const char *MainMenu[8] = {"1.Test Song #1\n", "2. Test Song #2\n"};
-
+const char *Welcome[3] = {"Welcome to the  Digital Piano.", "Can play, record Press 1 to", "continue."};
+const char *MainMenu[2] = {"1: Play stored songs.", "2: Record Song"};
+const char *SongMenu[8] = {"1.Test Song #1\n", "2. Test Song #2\n", "3. Previous Menu"};
+const char *RecordMenu[3] = {0};
+ 
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_time_cntcurr=0;
 unsigned char tmpA = 0x00;
-unsigned char ucMaxIdx = 3;
+unsigned char ucWelcIdx = 3;
+unsigned char ucMainIdx = 2;
 unsigned short usDspPrd = 2000;
 
 void TimerOn()
@@ -56,12 +59,13 @@ void TimerSet(unsigned long M)
 	_avr_time_cntcurr = _avr_timer_M;
 }
 
-enum Dsp_States{Dsp_Init, Dsp_Intro, Dsp_Choice, Dsp_Play, Dsp_Rec} Dsp_State;
+enum Dsp_States{Dsp_Init, Dsp_Intro, Dsp_Intro_Rel, Dsp_Choice, Dsp_Play, Dsp_Rec} Dsp_State;
 	
 void TckFct_Display()
 {
 	static unsigned short usCnt = 0;
 	static unsigned char ucIdx = 0;
+	//static unsigned char ucChoice = 0;  /* might need for storing user choice */
 	
 	tmpA = ~PINA;
 	switch(Dsp_State)
@@ -74,16 +78,16 @@ void TckFct_Display()
 			ucIdx++;
 			Dsp_State = Dsp_Intro;
 			break;
-		case Dsp_Intro:
+		case Dsp_Intro:	 // displaying menu and waiting for user input
 			if(tmpA == 0x01) // checking user pressing button1 
 			{
 				usCnt = 0;
 				ucIdx = 0;
-				Dsp_State = Dsp_Choice;
+				Dsp_State = Dsp_Intro_Rel;
 			}
 			else
 			{
-				if(ucIdx < ucMaxIdx)
+				if(ucIdx < ucWelcIdx)
 				{
 					if(usCnt < usDspPrd)
 					{
@@ -104,7 +108,49 @@ void TckFct_Display()
 				Dsp_State = Dsp_Intro;
 			}
 			break;
+		case Dsp_Intro_Rel:  // waiting for user to release button
+			if(tmpA == 0x00)
+			{
+				LCD_DisplayString(1, (const unsigned char*)MainMenu[ucIdx]);
+				ucIdx++;
+				Dsp_State = Dsp_Choice;
+			}
+			else
+			{
+				Dsp_State = Dsp_Intro_Rel;
+			}
+			break;
 		case Dsp_Choice:
+			if(tmpA == 0x01)
+			{
+				Dsp_State = Dsp_Play;
+			}
+			else if(tmpA == 0x02)
+			{
+				Dsp_State = Dsp_Rec;
+			}
+			else
+			{
+				if(ucIdx < ucMainIdx)
+				{
+					if(usCnt < usDspPrd)
+					{
+						usCnt++;
+					}
+					else
+					{
+						usCnt = 0;
+						LCD_DisplayString(1, (const unsigned char*)MainMenu[ucIdx]);
+						ucIdx++;
+					}
+				}
+				else
+				{
+					ucIdx = 0;
+					usCnt = 0;
+				}
+				Dsp_State = Dsp_Choice;
+			}
 			break;
 		case Dsp_Play:
 			break;
@@ -119,6 +165,8 @@ void TckFct_Display()
 		case Dsp_Init:
 			break;
 		case Dsp_Intro:
+			break;
+		case Dsp_Intro_Rel:
 			break;
 		case Dsp_Choice:
 			break;
@@ -140,7 +188,6 @@ void TckFct_RecordSound()
 /* function for getting data from eeprom */
 void GetData()
 {
-	
 }
 
 /* function for setting data from eeprom */
@@ -152,14 +199,16 @@ void SetData()
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0x00; PORTB = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 	
 	LCD_init();
 	TimerSet(1);
 	TimerOn();
 	Dsp_State = -1;
+	
+	SetData();
+	GetData();
 	
     while(1)
     {
