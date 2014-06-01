d5b16e601c0031b847bc65c84f94fd8d0d0f7e1b
btstevenson <bstevo30@gmail.com>
2014-05-31 17:00:44 -0700

    added functionality for playing and recording songs

--- Source/Digital_Piano.c
+++ Source/Digital_Piano.c
@@ -6,6 +6,8 @@
  */ 
 
 #include <avr/io.h>
+#include <stdlib.h>
+#include <string.h>
 #include "io.c"
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
@@ -14,6 +16,14 @@ volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_time_cntcurr=0;
 unsigned char tmpA = 0x00;
+unsigned char gucSongChoice = 0; // global for song choice, might not need later on
+
+struct SongInfo
+{
+	char szSongName[32];
+	unsigned short usSongLength; 
+};
+typedef struct SongInfo SongInfo_t;
 
 
 void TimerOn()
@@ -52,7 +62,7 @@ void TimerSet(unsigned long M)
 	_avr_time_cntcurr = _avr_timer_M;
 }
 
-enum Dsp_States{Dsp_Init, Dsp_Intro, Dsp_Intro_Rel, Dsp_Choice, Dsp_Play, Dsp_Rec} Dsp_State;
+enum Dsp_States{Dsp_Init, Dsp_Intro, Dsp_Intro_Rel, Dsp_Choice, Dsp_Choice_Rel, Dsp_Play, Dsp_Rec} Dsp_State;
 	
 void TckFct_Display()
 {
@@ -60,12 +70,14 @@ void TckFct_Display()
 	static unsigned char ucIdx = 0;
 	static unsigned char ucWelcIdx = 3;
 	static unsigned char ucMainIdx = 2;
-	unsigned short usDspPrd = 2000;
+	static unsigned char ucSongIdx = 3; // variable for size of songs stored on chip
+	static unsigned char ucRecIdx = 2;
+	static unsigned char ucChoice = 0;  /* might need for storing user choice */
+	static unsigned short usDspPrd = 2000;
 	const char *Welcome[3] = {"Welcome to the  Digital Piano.", "Can play, record Press 1 to", "continue."};
 	const char *MainMenu[2] = {"1: Play stored songs.", "2: Record Song"};
-	const char *SongMenu[8] = {"1.Test Song #1\n", "2. Test Song #2\n", "3. Previous Menu"};
-	const char *RecordMenu[3] = {0};
-	//static unsigned char ucChoice = 0;  /* might need for storing user choice */
+	const char *SongMenu[8] = {"1.Test Song #1\n", "2. Test Song #2\n", "3. Previous Menu"}; /* will add predetermined song names here */ 
+	const char *RecordMenu[3] = {"Push and release 1 to start", "recording the song played"};
 	
 	tmpA = ~PINA;
 	switch(Dsp_State)
@@ -120,14 +132,20 @@ void TckFct_Display()
 				Dsp_State = Dsp_Intro_Rel;
 			}
 			break;
-		case Dsp_Choice:
-			if(tmpA == 0x01)
+		case Dsp_Choice: // getting user choice to record or play
+			if(tmpA == 0x01 || tmpA == 0x02)
 			{
-				Dsp_State = Dsp_Play;
-			}
-			else if(tmpA == 0x02)
-			{
-				Dsp_State = Dsp_Rec;
+				usCnt = 0;
+				ucIdx = 0;
+				if(tmpA == 0x01)
+				{
+					ucChoice = 0;
+				}
+				else
+				{
+					ucChoice = 1;
+				}
+				Dsp_State = Dsp_Choice_Rel;
 			}
 			else
 			{
@@ -152,9 +170,107 @@ void TckFct_Display()
 				Dsp_State = Dsp_Choice;
 			}
 			break;
+		case Dsp_Choice_Rel: // waiting for user to release button
+			if(tmpA == 0x00)
+			{
+				if(ucChoice == 0)
+				{
+					LCD_DisplayString(1, (unsigned char*)SongMenu[ucIdx]);
+					ucIdx++;
+					Dsp_State = Dsp_Play;
+				}
+				else
+				{
+					LCD_DisplayString(1, (unsigned char*)RecordMenu[ucIdx]);
+					ucIdx++;
+					Dsp_State = Dsp_Rec;
+				}
+			}
+			else
+			{
+				Dsp_State = Dsp_Choice_Rel;
+			}
+			break;
 		case Dsp_Play:
+			//need to create switch statement for choice of song or previous menu
+			if(tmpA != 0x00)
+			{
+				// testing with two songs only at first, will add more later
+				switch(tmpA)
+				{
+					case 1:
+						gucSongChoice = 1;
+						break;
+					case 2:
+						gucSongChoice = 2;
+						break;
+					case 4:
+						//gucSongChoice = 3;
+						break;
+					case 8:
+						break;
+					case 16:
+						break;
+					case 32:
+						break;
+					case 64:
+						break;
+					case 128:
+						break;
+					default:
+						break;
+				}
+			}
+			else
+			{
+				if(ucIdx < ucSongIdx)
+				{
+					if(usCnt < usDspPrd)
+					{
+						usCnt++;
+					}
+					else
+					{
+						usCnt = 0;
+						LCD_DisplayString(1, (unsigned char*)SongMenu[ucIdx]);
+						ucIdx++;
+					}
+				}
+				else
+				{
+					ucIdx = 0;
+					usCnt = 0;
+				}
+				Dsp_State = Dsp_Play;
+			}
 			break;
 		case Dsp_Rec:
+			if(tmpA == 0x01)
+			{
+				// need to decide on how to store record state for next state machine
+			}
+			else
+			{
+				if(ucIdx < ucRecIdx)
+				{
+					if(usCnt < usDspPrd)
+					{
+						usCnt++;
+					}
+					else
+					{
+						usCnt++;
+						LCD_DisplayString(1, (unsigned char*)RecordMenu[ucIdx]);
+						ucIdx++;
+					}
+				}
+				else
+				{
+					ucRecIdx = 0;
+					usCnt = 0;
+				}
+				Dsp_State = Dsp_Rec;
+			}
 			break;
 		default:
 			break;
@@ -179,19 +295,56 @@ void TckFct_Display()
 	}	
 }
 
+enum Ply_States{Ply_Init, Ply_Get, Ply_Play} Ply_State;
+
+void TckFct_PlaySong()
+{
+	SongInfo_t SongHold;
+	memset(&SongHold, 0, sizeof(SongInfo_t));
+	
+	switch(Ply_State)
+	{
+		case Ply_Init:
+			break;
+		case Ply_Get:
+			break;
+		case Ply_Play:
+			break;
+		default:
+			break;
+	}
+	switch(Ply_State)
+	{
+		case Ply_Init:
+			break;
+		case Ply_Get:
+			break;
+		case Ply_Play:
+			break;
+		default:
+			break;		
+	}	
+}
+
 /* function for recording input from user */
 void TckFct_RecordSound()
 {
 	
 }
 
-/* function for getting data from eeprom */
+/* function for getting recorded song from eeprom */
 void GetData()
 {
 }
 
-/* function for setting data from eeprom */
-void SetData()
+/* function for setting recorded song into eeprom */
+void SetData(unsigned char* ucTemp, unsigned char ucSongNum)
+{
+	
+}
+
+//basic prototype for talking to other micro controller
+void UsartTalk()
 {
 	
 }
@@ -200,6 +353,7 @@ int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 	
 	LCD_init();
@@ -207,8 +361,8 @@ int main(void)
 	TimerOn();
 	Dsp_State = -1;
 	
-	SetData();
-	GetData();
+	//SetData();
+	//GetData();
 	
     while(1)
     {
